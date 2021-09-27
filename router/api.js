8e608871d02f645a46ba215253e6111acebfd4c4
zhulu <zhululu@weihaizixun.com>
2021-09-27 15:38:02 +0800

    如果音频链接不是完整版，说明当前设置的cookie不是VIP会员

--- router/api.js
+++ router/api.js
@@ -1,6 +1,8 @@
 const Router = require('koa-router')
 const router = new Router()
 
+const fs = require('fs')
+
 const getAudioUrl = require('../lib/xmly')
 const qqmusic = require('../lib/qqmusic')
 const migu = require('../lib/migu')
@@ -33,6 +35,34 @@ router.get('/xmly', async (ctx) => {
 
 /**
  * @swagger
+ * /api/xmly:
+ *   post:
+ *     summary: 设置喜玛拉雅VIP的登录凭据
+ *     description: 必须要VIP账号的cookie，不然没啥用，会在项目根目录新建xmly.cookie的文件
+ *     tags:
+ *       - API
+ *     parameters:
+ *       - name: cookie
+ *         in: formData
+ *         required: true
+ *         description: 喜马拉雅会员cookie
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: 成功获取
+ */
+router.post('/xmly', async (ctx) => {
+    const { cookie } = ctx.request.body
+    // 写入cookie
+    fs.writeFileSync('xmly.cookie', cookie.trim());
+    // 测试是否可用
+    const body = await getAudioUrl(244802645)
+    body.msg = '如果音频链接不是完整版，说明当前设置的cookie不是VIP会员'
+    ctx.body = body
+})
+
+/**
+ * @swagger
  * /api/search:
  *   get:
  *     summary: 搜索音乐资源
@@ -53,12 +83,12 @@ router.get('/search', async (ctx) => {
     const { key } = ctx.query
     let data = await qqmusic(key)
     if (data) {
-        return ctx.body = { code: 0, data, msg: '在QQ音乐中找到' }        
+        return ctx.body = { code: 0, data, msg: '在QQ音乐中找到' }
     }
 
     data = await migu(key)
     if (data) {
-        return ctx.body = { code: 0, data, msg: '在咪咕音乐中找到' }        
+        return ctx.body = { code: 0, data, msg: '在咪咕音乐中找到' }
     }
 
     data = await kugou(key)
@@ -70,7 +100,7 @@ router.get('/search', async (ctx) => {
     if (data) {
         return ctx.body = { code: 0, data, msg: '在酷我音乐中找到' }
     }
-    
+
     ctx.body = { code: 1, msg: '找不到音乐' }
 })
 
